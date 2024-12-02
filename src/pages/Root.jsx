8b8fdb9eca9d39873c89126499be173ac59435aa
Hariharan Mohan <haranhari350@gmail.com>
2024-12-01 20:39:05 -0600

    routes defined using modern pattern; createBrowserRouter and RouterProvider

---
+++ src/pages/Root.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Home from "./Home";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function Root() {
+  return (
+    <div className="min-h-screen bg-primary flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
